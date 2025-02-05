2b6f54044855220eca62578165e3d1f0937869f0
lucasmodin <lumo0002@stud.kea.dk>
2025-02-05 12:58:22 +0100

    Initial commit

---
+++ src/main/java/org/example/githubactionstart/GitHubActionStartApplication.java
@@ -0,0 +1,13 @@
+package org.example.githubactionstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubActionStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubActionStartApplication.class, args);
+    }
+
+}
